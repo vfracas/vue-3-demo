08ca213b8dedff0c7524f47185df62926e54ebdc
Vincent <vincent.fracas@gmail.com>
2023-04-23 22:54:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import NavBar from "@/components/NavBar.vue";
+</script>
+
+<template>
+  <header>
+    <div class="min-h-screen w-screen flex flex-col items-center">
+      <NavBar />
+      <div class="h-full w-full flex justify-center items-center max-w-7xl p-4">
+        <RouterView v-slot="{ Component, route }">
+          <component :is="Component" :key="route.path" />
+        </RouterView>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="w-screen h-16 bg-gray-200 flex items-center justify-center">
+    <nav class="max-w-7xl w-full flex justify-around items-center">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+</template>
